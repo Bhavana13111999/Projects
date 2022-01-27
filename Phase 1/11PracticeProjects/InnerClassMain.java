40b96a691fce07e4fb96c9a0b9a81c9a9a11f99d
Bhavana13111999 <70712124+Bhavana13111999@users.noreply.github.com>
2022-01-27 17:08:00 +0530

    Add files via upload

---
+++ Phase 1/11PracticeProjects/InnerClassMain.java
@@ -0,0 +1,20 @@
+//7th Program//
+
+package com.Bhavana.sl.Assignements;
+ class OuterClass {
+	  int x = 10;
+
+	  class InnerClass {
+	    int y = 5;
+	  }
+	}
+public class InnerClassMain {
+	  public static void main(String[] args) {
+		    OuterClass myOuter = new OuterClass();
+		    OuterClass.InnerClass myInner = myOuter.new InnerClass();
+		    System.out.println(myInner.y + myOuter.x);
+		  }
+		}
+
+
+
---
+++ Phase 1/11PracticeProjects/RegularExpressions.java
@@ -0,0 +1,15 @@
+//10th Program//
+
+package com.Bhavana.sl.Assignements;
+import java.util.regex.Pattern;
+public class RegularExpressions {
+
+		public static void main(String args[])
+		{
+            System.out.println(Pattern.matches("Bhavana", "Bhavana"));// it returns true as the strings match
+			System.out.println(Pattern.matches("Bhavana", "Bhav"));// it returns false as the strings does not match
+
+		}
+	}
+
+
